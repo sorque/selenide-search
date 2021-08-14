2bddfb4d2c81968c72d7bc6b374e24f3b3aa76e4
m.korovin <m.korovin@corp.mail.ru>
2021-08-14 19:37:14 +0700

    testPull

--- src/test/java/google/com/FirstTest.java
+++ src/test/java/google/com/FirstTest.java
@@ -1,15 +1,34 @@
 package google.com;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class FirstTest {
 
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("this is beforAll message");
+    }
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("this is introduction");
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("this is conclusion");
+    }
+
     @Test
     void firstTest() {
-        System.out.println("Hello there");
+        System.out.println("some test here");
         Assertions.assertTrue( true);
 
     }
+    @Test
+    void secondTest() {
+        System.out.println("another test here");
+        Assertions.assertTrue( true);
 
+    }
 }
