96dac7bff044f1a974df2b3fe55b5c4493269d46
m.korovin <m.korovin@corp.mail.ru>
2021-08-14 19:13:36 +0700

    some tests

---
+++ src/test/java/google/com/FirstTest.java
@@ -0,0 +1,15 @@
+package google.com;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+
+    @Test
+    void firstTest() {
+        System.out.println("Hello there");
+        Assertions.assertTrue( true);
+
+    }
+
+}
