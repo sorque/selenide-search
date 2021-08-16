e77c67b2045a272ec3ec76f8789aff332bdd78ae
m.korovin <m.korovin@corp.mail.ru>
2021-08-16 16:24:32 +0700

    added search test and check

--- src/test/java/demoqa/com/FieldTest.java
+++ src/test/java/demoqa/com/FieldTest.java
@@ -1,16 +1,39 @@
 package demoqa.com;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class FieldTest {
 
+    @BeforeAll
+    static void setup() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.startMaximized = true;
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("done");
+    }
+
     @Test
     void positiveFieldTest() {
-        open("https://demoqa.com/text-box");
+        open("/text-box");
+        $("#userName").setValue("Mike");
+        $("#userEmail").setValue("sdd@corp.ru");
+        $("#currentAddress", 0).setValue("someStreet");
+        $("#permanentAddress").setValue("anotherAdress");
+        $("#submit").click();
 
-        System.out.println("this is introduction");
-
+        $("#name").shouldHave(text("Mike"));
+        $("#email").shouldHave(text("sdd@corp.ru"));
+        $("#currentAddress", 1).shouldHave(text("someStreet"));
+        $("#permanentAddress", 1).shouldHave(text("anotherAdress"));
     }
 }
